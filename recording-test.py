465602eefe433a481da9a66670fd65989aaf0571
winkingcat <dankercher@gmail.com>
2022-03-04 15:31:43 -0500

    resize

--- recording-test.py
+++ recording-test.py
@@ -14,9 +14,9 @@ emotion_count, angry, disgust, fear, happy, sad, surprise, neutral = 0, 0, 0, 0,
 
 # Comment out 1 vid_path line below for test
 #vid_path = "vids/exp_vid_multHead_test_1.mp4"
-#vid_path = "vids/exp_vid_test_1.mp4"
+vid_path = "vids/exp_vid_test_1.mp4"
 #vid_path = "vids/exp_vid_zoom_test_1.mp4.mp4"
-vid_path ="vids/mini_zoom_test.mp4"
+#vid_path ="vids/mini_zoom_test.mp4"
 
 # Text file for output
 f = open("output/emotional_analysis_output.txt", 'w')
@@ -29,33 +29,35 @@ while(True):
       
     # Capture the video frame
     # by frame
-    ret, frameRaw = vid.read()
+    ret, raw_frame = vid.read()
     
-    # mirror cam (optional) If unused, comment out and rename vid read line "frame"
-    frame = cv2.flip(frameRaw,1)
-
     # Deepface begin
     if(ret):
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        # Manip frame: reduce, flip, recolor
+        reduced_frame = cv2.resize(raw_frame, (0,0), fx=0.5, fy=0.5) 
+        flipped_frame = cv2.flip(reduced_frame,1)
+        grayed_frame = cv2.cvtColor(flipped_frame, cv2.COLOR_BGR2GRAY)
+
+        # Get faces
+        faces = face_cascade.detectMultiScale(grayed_frame, 1.3, 5)
         for (x,y,w,h) in faces:
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2) #draw rectangle to main image
+            cv2.rectangle(flipped_frame,(x,y),(x+w,y+h),(255,0,0),2) #draw rectangle to main image
 
-        detected_face = frame[int(y):int(y+h), int(x):int(x+w)] #crop detected face
-        detected_face = cv2.cvtColor(detected_face, cv2.COLOR_BGR2GRAY) #transform to gray scale
-        detected_face = cv2.resize(detected_face, (48, 48)) #resize to 48x48
+            detected_face = flipped_frame[int(y):int(y+h), int(x):int(x+w)] #crop detected face
+            detected_face = cv2.cvtColor(detected_face, cv2.COLOR_BGR2GRAY) #transform to gray scale
+            detected_face = cv2.resize(detected_face, (48, 48)) #resize to 48x48
 
-        img_pixels = image.img_to_array(detected_face)
-        img_pixels = np.expand_dims(img_pixels, axis = 0)
+            img_pixels = image.img_to_array(detected_face)
+            img_pixels = np.expand_dims(img_pixels, axis = 0)
 
-        img_pixels /= 255 #pixels are in scale of [0, 255]. normalize all pixels in scale of [0, 1]
+            img_pixels /= 255 #pixels are in scale of [0, 255]. normalize all pixels in scale of [0, 1]
 
-        predictions = model.predict(img_pixels) #store probabilities of 7 expressions
-    
-        #find max indexed array 0: angry, 1:disgust, 2:fear, 3:happy, 4:sad, 5:surprise, 6:neutral
-        max_index = np.argmax(predictions[0])
+            predictions = model.predict(img_pixels) #store probabilities of 7 expressions
+        
+            #find max indexed array 0: angry, 1:disgust, 2:fear, 3:happy, 4:sad, 5:surprise, 6:neutral
+            max_index = np.argmax(predictions[0])
 
-        emotion = emotions[max_index]
+            emotion = emotions[max_index]
 
         # log max emotion
         emotion_count += 1
@@ -76,11 +78,11 @@ while(True):
 
 
         #write emotion text above rectangle
-        cv2.putText(frame, emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+        cv2.putText(flipped_frame, emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
         # Deepface end
 
         # Display the resulting frame
-        cv2.imshow('frame', frame)
+        cv2.imshow('frame', flipped_frame)
         
         # the 'q' button is set as the
         # quitting button you may use any
@@ -95,32 +97,32 @@ vid.release()
 # Destroy all the windows
 cv2.destroyAllWindows()
 
-# Write emotion log output and close file
-f.write("Emotions Counted: {}\n".format(emotion_count))
-f.write("---Angry---\nCount: {}\nPercent:{}\n\n".format(angry, angry/emotion_count*100))
-f.write("---Disgust---\nCount: {}\nPercent:{}\n\n".format(disgust, disgust/emotion_count*100))
-f.write("---Fear---\nCount: {}\nPercent:{}\n\n".format(fear, fear/emotion_count*100))
-f.write("---Happy---\nCount: {}\nPercent:{}\n\n".format(happy, happy/emotion_count*100))
-f.write("---Sad---\nCount: {}\nPercent:{}\n\n".format(sad, sad/emotion_count*100))
-f.write("---Surprise---\nCount: {}\nPercent:{}\n\n".format(surprise, surprise/emotion_count*100))
-f.write("---Neutral---\nCount: {}\nPercent:{}\n\n".format(neutral, neutral/emotion_count*100))
+# # Write emotion log output and close file
+# f.write("Emotions Counted: {}\n".format(emotion_count))
+# f.write("---Angry---\nCount: {}\nPercent:{}\n\n".format(angry, angry/emotion_count*100))
+# f.write("---Disgust---\nCount: {}\nPercent:{}\n\n".format(disgust, disgust/emotion_count*100))
+# f.write("---Fear---\nCount: {}\nPercent:{}\n\n".format(fear, fear/emotion_count*100))
+# f.write("---Happy---\nCount: {}\nPercent:{}\n\n".format(happy, happy/emotion_count*100))
+# f.write("---Sad---\nCount: {}\nPercent:{}\n\n".format(sad, sad/emotion_count*100))
+# f.write("---Surprise---\nCount: {}\nPercent:{}\n\n".format(surprise, surprise/emotion_count*100))
+# f.write("---Neutral---\nCount: {}\nPercent:{}\n\n".format(neutral, neutral/emotion_count*100))
 
-# log max emotion
-dom_emo = max(angry, disgust, fear, happy, sad, surprise, neutral)
-if(dom_emo == angry):
-    dom_emo_string = "Angry"
-elif(dom_emo == disgust):
-    dom_emo_string = "Disgust"
-elif(dom_emo == fear):
-    dom_emo_string = "Fear"
-elif(dom_emo == happy):
-    dom_emo_string = "Happy"
-elif(dom_emo == sad):
-    dom_emo_string = "Sad"
-elif(dom_emo == surprise):
-    dom_emo_string = "Surprise"
-elif(dom_emo == neutral):
-    dom_emo_string = "Neutral"
+# # log max emotion
+# dom_emo = max(angry, disgust, fear, happy, sad, surprise, neutral)
+# if(dom_emo == angry):
+#     dom_emo_string = "Angry"
+# elif(dom_emo == disgust):
+#     dom_emo_string = "Disgust"
+# elif(dom_emo == fear):
+#     dom_emo_string = "Fear"
+# elif(dom_emo == happy):
+#     dom_emo_string = "Happy"
+# elif(dom_emo == sad):
+#     dom_emo_string = "Sad"
+# elif(dom_emo == surprise):
+#     dom_emo_string = "Surprise"
+# elif(dom_emo == neutral):
+#     dom_emo_string = "Neutral"
 
-f.write("Dominant Emotion: {}\n".format(dom_emo_string))
-f.close()
+# f.write("Dominant Emotion: {}\n".format(dom_emo_string))
+# f.close()
