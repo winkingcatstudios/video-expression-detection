dac43ead6ba066bf42a2e0b538b3e6a30bee4e60
winkingcat <dankercher@gmail.com>
2022-03-08 21:37:02 -0500

    ssd and emotions working for video feed

--- recording-test-ssd.py
+++ recording-test-ssd.py
@@ -1,9 +1,10 @@
 import cv2
-from keras.preprocessing import image
 import numpy as np
+import pandas as pd
+from keras.preprocessing import image as kerasImage
 from keras.models import model_from_json
 
-face_cascade = cv2.CascadeClassifier('C:\Python39\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml')
+detector = cv2.dnn.readNetFromCaffe("models-and-weights/deploy.prototxt" , "models-and-weights/res10_300x300_ssd_iter_140000.caffemodel")
 
 model = model_from_json(open("models-and-weights/facial_expression_model_structure.json", "r").read())
 model.load_weights('models-and-weights/facial_expression_model_weights.h5') #load weights
@@ -29,33 +30,53 @@ while(True):
       
     # Capture the video frame
     # by frame
-    ret, raw_frame = vid.read()
+    ret, image = vid.read()
     
     # Deepface begin
     if(ret):
         # Manip frame: reduce, flip, recolor
-        reduced_frame = cv2.resize(raw_frame, (0,0), fx=0.5, fy=0.5) 
-        flipped_frame = cv2.flip(reduced_frame,1)
-        grayed_frame = cv2.cvtColor(flipped_frame, cv2.COLOR_BGR2GRAY)
+        base_img = image.copy()
+        original_size = image.shape
+        target_size = (300, 300)
+        image = cv2.resize(image, target_size)
+        aspect_ratio_x = (original_size[1] / target_size[1])
+        aspect_ratio_y = (original_size[0] / target_size[0])
 
         # Get faces
-        faces = face_cascade.detectMultiScale(grayed_frame, 1.3, 5)
-        #print(faces.size/4)    debug number of faces currently found
+        imageBlob = cv2.dnn.blobFromImage(image = image)
         
-        for (x,y,w,h) in faces:
-            cv2.rectangle(flipped_frame,(x,y),(x+w,y+h),(255,0,0),2) #draw rectangle to main image
+        detector.setInput(imageBlob)
+        detections = detector.forward()
 
-            detected_face = flipped_frame[int(y):int(y+h), int(x):int(x+w)] #crop detected face
+        detections_df = pd.DataFrame(detections[0][0]
+            , columns = ["img_id", "is_face", "confidence", "left", "top", "right", "bottom"])
+
+        detections_df = detections_df[detections_df['is_face'] == 1] #0: background, 1: face
+        detections_df = detections_df[detections_df['confidence'] >= 0.25]  # default: 0.90
+
+
+        for i, instance in detections_df.iterrows():
+            #print(instance)
+            
+            confidence_score = str(round(100*instance["confidence"], 2))+" %"
+            
+            left = int(instance["left"] * 300)
+            bottom = int(instance["bottom"] * 300)
+            right = int(instance["right"] * 300)
+            top = int(instance["top"] * 300)
+            
+            #high resolution
+            detected_face = base_img[int(top*aspect_ratio_y):int(bottom*aspect_ratio_y), int(left*aspect_ratio_x):int(right*aspect_ratio_x)]
             detected_face = cv2.cvtColor(detected_face, cv2.COLOR_BGR2GRAY) #transform to gray scale
             detected_face = cv2.resize(detected_face, (48, 48)) #resize to 48x48
 
-            img_pixels = image.img_to_array(detected_face)
+            img_pixels = kerasImage.img_to_array(detected_face)
             img_pixels = np.expand_dims(img_pixels, axis = 0)
 
             img_pixels /= 255 #pixels are in scale of [0, 255]. normalize all pixels in scale of [0, 1]
 
             predictions = model.predict(img_pixels) #store probabilities of 7 expressions
-        
+
             #find max indexed array 0: angry, 1:disgust, 2:fear, 3:happy, 4:sad, 5:surprise, 6:neutral
             max_index = np.argmax(predictions[0])
 
@@ -78,13 +99,15 @@ while(True):
             elif(emotion == "neutral"):
                 neutral +=1
 
+            if detected_face.shape[0] > 0 and detected_face.shape[1] > 0:
 
-            # write emotion text above rectangle
-            cv2.putText(flipped_frame, emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-        # Deepface end
+                #high resolution
+                cv2.putText(base_img, emotion, (int(left*aspect_ratio_x), int(top*aspect_ratio_y-10)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+                cv2.putText(base_img, confidence_score, (int(left*aspect_ratio_x), int(bottom*aspect_ratio_y+15)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+                cv2.rectangle(base_img, (int(left*aspect_ratio_x), int(top*aspect_ratio_y)), (int(right*aspect_ratio_x), int(bottom*aspect_ratio_y)), (255,0,0),2) #draw rectangle to main image
 
         # Display the resulting frame
-        cv2.imshow('frame', flipped_frame)
+        cv2.imshow('frame', base_img)
         
         # the 'q' button is set as the
         # quitting button you may use any
