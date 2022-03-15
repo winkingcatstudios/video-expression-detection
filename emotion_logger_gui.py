60487add5b09eaaeb38166844f7c1d17709b3ab9
winkingcat <dankercher@gmail.com>
2022-03-14 21:01:41 -0400

    Gui now shows output as well as writing to file

--- emotion_logger_gui.py
+++ emotion_logger_gui.py
@@ -18,16 +18,30 @@ def select_file():
         ('Compatible files', '*.mp4 *.avi *.mov *.wmv'), # Zoom defaults to .mp4 but other options are allowable for this program
     )
 
+    load_label.pack(expand=True)
+
     filename = fd.askopenfilename(
         title='Open a file',
         initialdir='/',
         filetypes=filetypes)
     
     emotion_logger.set_vid_path(filename)
+    load_label.config(text = "Loading Complete")
     run_button.pack(expand=True)
 
 def logger_run():
     emotion_logger.run()
+    root.geometry('300x500')
+    result_label.config(text='-Results-'
+    '\nAngry: ' + str(emotion_logger.angry) + 
+    '\nDisgust: ' + str(emotion_logger.disgust) + 
+    '\nFear: ' + str(emotion_logger.fear) + 
+    '\nHappy: ' + str(emotion_logger.happy) + 
+    '\nSad: ' + str(emotion_logger.sad) + 
+    '\nSurprise: ' + str(emotion_logger.surprise) + 
+    '\nNeutral: ' + str(emotion_logger.neutral) + 
+    '\nDominant Emotion: ' + str(emotion_logger.dom_emo_string))
+    result_label.pack(expand=True)
 
 # instruction label
 inst_label = tk.Label(
@@ -44,15 +58,32 @@ open_button = ttk.Button(
     command=select_file
 )
 
-# open button
+# loading label
+load_label = tk.Label(
+    root,
+    text='Loading...',
+    width=30,
+    font=('times', 12)
+)  
+
+# run button
 run_button = ttk.Button(
     root,
     text='Run Emotion Logger',
-    command=logger_run
+    command=logger_run,
 )
+
+# result label
+result_label = tk.Label(
+    root,
+    text='-Results-',
+    width=30,
+    font=('times', 18, 'bold')
+)  
 
 inst_label.pack(expand=True)
 open_button.pack(expand=True)
+
 
 
 # run the application
